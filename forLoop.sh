7aada3068cf69724252a87b05604ee485b43ab32
ramakrishna <99ramakrishna.b@gmail.com>
2020-02-28 18:37:50 +0000

    all

---
+++ forLoop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ips=($1 $2 $3)
+
+echo ${ips[@]}
+
+echo ${#ips[@]}
+
+echo  ${ips[1]}
+
+
---
+++ gitpush.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+filename=$1
+branchname=$2
+flag=$3
+commitmsg=$4
+
+if [[ -f $filename || -d $filename ]]
+then
+   git add $filename
+    
+   if [ $? == 0  ]
+   then 
+     
+      git status
+      if [ $? == 0 ]
+      then 
+	 echo "*********************************  successfully added *************************"     
+
+	 git commit $flag  "$commitmsg"
+         echo "********************************* successfully commited **********************"
+	 git config credential.helper store
+
+	 git push origin $branchname
+         
+	
+	 
+      else
+         echo "something went wrong"	      
+      fi	 
+
+   else 
+      echo "something went wrong"   
+
+   fi
+else 
+   echo "$filename is not found"
+fi
+
+
