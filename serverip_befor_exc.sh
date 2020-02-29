6e532851b47759aee6db9d3858b2d44a141a7846
ramakrishna <99ramakrishna.b@gmail.com>
2020-02-29 13:04:17 +0530

    ips.txt

---
+++ serverip_befor_exc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+for ping in  $(cat ips.txt)
+do
+  ping $ping -c  2
+  if [ $?  == 0 ]
+  then
+     echo  " "
+     echo  " "
+     echo  "********** $ping  is  running ****************"
+     echo  " "
+     echo  " "
+     echo  " "
+     echo  " "
+     echo  " "
+     sleep 3
+  else
+     echo  " "
+     echo  " "
+     echo  "********* $ping  is  Down *********************"
+     echo  " "
+     echo  " "
+     echo  " "
+     echo  " "
+     echo  " "
+     sleep 3
+  fi
+done
+
